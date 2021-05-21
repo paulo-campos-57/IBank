5f9440b4b8d1aefca01bb34542c0a3ef1cfa054e
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2021-05-21 08:35:34 -0300

    Create Conta.java

---
+++ Conta.java
@@ -0,0 +1,49 @@
+package BankProject;
+
+abstract class Conta {
+
+	private int numero;
+	private String agencia;
+	private Cliente dono; 	
+	private double saldo;
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public Cliente getDono() {
+		return dono;
+	}
+
+	public void setDono(Cliente dono) {
+		this.dono = dono;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public abstract void exibirSaldo();
+	
+	public abstract void transferencia(Conta contaDestino, double valor);
+	
+	public abstract void sacar(double valor);
+
+	public abstract void depositar(double valor);
+	
+}
