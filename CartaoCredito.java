f04bd6815ed82864f2ebd4e8ede85d3f87ed9c1e
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2021-05-21 08:40:03 -0300

    Create CartaoCredito.java

---
+++ CartaoCredito.java
@@ -0,0 +1,15 @@
+package IBank;
+
+public class CartaoCredito extends Cartao{
+
+	private double limite;
+
+	public double getLimite() {
+		return limite;
+	}
+
+	public void setLimite(double limite) {
+		this.limite = limite;
+	}
+	
+}
